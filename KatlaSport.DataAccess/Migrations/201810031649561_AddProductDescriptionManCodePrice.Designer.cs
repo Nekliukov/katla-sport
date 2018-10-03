a4cee9cf10dd37861432cad80ea69a5a620ed9df
Nekliukov <roman.nekliukov@gmail.com>
2018-10-03 20:14:08 +0300

    Add Description, ManufacturerCode and Price property to CatalogueProduct and UpdateProductRequest model. Create a new migration AddProductDescriptionManCodePrice. Add new rules to UpdateProductRequestValidator.

--- KatlaSport.DataAccess/Migrations/201810031649561_AddProductDescriptionManCodePrice.Designer.cs
+++ KatlaSport.DataAccess/Migrations/201810031649561_AddProductDescriptionManCodePrice.Designer.cs
@@ -2,17 +2,18 @@
 namespace KatlaSport.DataAccess.Migrations
 {
     using System.CodeDom.Compiler;
+    using System.Data.Entity.Migrations;
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class AddCategoryDescription : IMigrationMetadata
+    public sealed partial class AddProductDescriptionManCodePrice : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddCategoryDescription));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AddProductDescriptionManCodePrice));
         
         string IMigrationMetadata.Id
         {
-            get { return "201810031354465_AddCategoryDescription"; }
+            get { return "201810031649561_AddProductDescriptionManCodePrice"; }
         }
         
         string IMigrationMetadata.Source
