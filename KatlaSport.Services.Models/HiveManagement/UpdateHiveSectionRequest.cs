d5a76b6fe0404eea606bb8ba133533b388f19df0
Nekliukov <roman.nekliukov@gmail.com>
2018-10-08 04:29:59 +0300

    Add/Update/Delete async methods for HiveSectionController and HiveSectionService were realised

---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represents a request for creating and updating a hive section.
+    /// </summary>
+    public class UpdateHiveSectionRequest
+    {
+        /// <summary>
+        /// Gets or sets a store hive section name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a store hive section code.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the store hive identifier.
+        /// </summary>
+        public int StoreHiveId { get; set; }
+        /* PS: This property needs here to set the store hive id because new section
+         doesn't know with which hive it must be connected */
+    }
+}
